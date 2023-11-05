ca4dbb76888c1498e54ba6a89d52646a7a2eea9b
nonyx777 <nahomretebo@gmail.com>
2023-11-05 17:45:46 +0300

    more calls per frame(distanceResolver)

--- Assets/RCW/DistanceJoint.cs
+++ Assets/RCW/DistanceJoint.cs
@@ -7,30 +7,40 @@ public class DistanceJoint : MonoBehaviour
 {
     [Header("Distance-Joint Parameters")]
     [SerializeField] private float length;
-    void Start(){
+    void Start()
+    {
 
     }
-    
-    void Update(){
-        for(int i = 0; i < HandleSegments.segments.Count; i++){
-            if(i+1 >= HandleSegments.segments.Count)
-                return;
-            int j = i+1;
-            Vector3 offsetVec = distanceResolver(HandleSegments.segments[i].transform.position,
-                                HandleSegments.segments[j].transform.position);
 
-            if(HandleSegments.segments[i].GetComponent<SegmentPhysics>().id != 0)
-                HandleSegments.segments[i].transform.position += offsetVec;
-            HandleSegments.segments[j].transform.position += -offsetVec;
-        }
+    void Update()
+    {
+        for(int i = 0; i < 10; i++)
+            handleResolver();
     }
 
-    Vector3 distanceResolver(Vector3 pos1, Vector3 pos2){
+    Vector3 distanceResolver(Vector3 pos1, Vector3 pos2)
+    {
         Vector3 displacement = pos1 - pos2;
         float distance = Vector3.Magnitude(displacement);
-        float offsetFloat = (length - distance)/2f;
+        float offsetFloat = (length - distance) / 2f;
         Vector3 offsetVec = displacement.normalized * offsetFloat;
 
         return offsetVec;
+    }
+
+    void handleResolver()
+    {
+        for (int i = 0; i < HandleSegments.segments.Count; i++)
+        {
+            if (i + 1 >= HandleSegments.segments.Count)
+                return;
+            int j = i + 1;
+            Vector3 offsetVec = distanceResolver(HandleSegments.segments[i].transform.position,
+                                HandleSegments.segments[j].transform.position);
+
+            if (HandleSegments.segments[i].GetComponent<SegmentPhysics>().id != 0)
+                HandleSegments.segments[i].transform.position += offsetVec;
+            HandleSegments.segments[j].transform.position += -offsetVec;
+        }
     }
 }
