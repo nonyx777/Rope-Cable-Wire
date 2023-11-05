a05c28ce4aef7ffb044fc4baaf6e54862735e7a3
nonyx777 <nahomretebo@gmail.com>
2023-11-05 13:14:28 +0300

    set spawn position

--- Assets/RCW/HandleSegments.cs
+++ Assets/RCW/HandleSegments.cs
@@ -8,6 +8,7 @@ public class HandleSegments : MonoBehaviour
     [Header("Rope Parameters")]
     [SerializeField] private GameObject Segment;
     [SerializeField] private int amount = 1;
+    [SerializeField] private Transform spawn_position;
     [SerializeField] public static List<GameObject> segments = new List<GameObject>();
     GameObject segment_tempo;
 
@@ -17,6 +18,8 @@ public class HandleSegments : MonoBehaviour
             segment_tempo.GetComponent<SegmentPhysics>().id = segments.Count;
             if(segments.Count >= 1)
                 segment_tempo.transform.position = positionSegment();
+            else
+                segment_tempo.transform.position = spawn_position.position;
             segments.Add(segment_tempo);
         }
     }
