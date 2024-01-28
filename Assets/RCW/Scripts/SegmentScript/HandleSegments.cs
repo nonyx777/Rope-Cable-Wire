16f4504492c660039fc9c94bfb5f8bacbaba328a
nonyx777 <nahomretebo@gmail.com>
2024-01-28 20:39:57 +0300

    new approach to creating and positioning segments, automatic assignemnt of number of segments

--- Assets/RCW/Scripts/SegmentScript/HandleSegments.cs
+++ Assets/RCW/Scripts/SegmentScript/HandleSegments.cs
@@ -7,32 +7,44 @@ public class HandleSegments : MonoBehaviour
 {
     [Header("Rope Parameters")]
     [SerializeField] private GameObject Segment;
-    [SerializeField] private int amount = 1;
-    [SerializeField] private Transform spawn_position;
+    [SerializeField] private Transform spawn_start;
+    [SerializeField] private Transform spawn_end;
     [SerializeField] private GameObject segment_parent;
     public static List<GameObject> segments = new List<GameObject>();
     GameObject segment_tempo;
 
     public void addSegment()
     {
-        for (int i = 0; i < amount; i++)
-        {
+        //depends on whether there is a segment before or if it's the first
+        Vector3 spawn_origin = segments.Count > 0 ? 
+                segments[segments.Count - 1].transform.position : 
+                spawn_start.position;
+
+        //direction in which the segments will spawn towards
+        //and other required vectors
+        Vector3 displacement = spawn_end.position - spawn_origin;
+        Vector3 spawn_direction = displacement.normalized;
+        int distance = (int) Vector3.Magnitude(displacement);
+
+        int num_of_segments = distance / (int) Segment.transform.localScale.y;
+
+        for(int i = 0; i < num_of_segments; i++){
             segment_tempo = Instantiate(Segment);
             segment_tempo.GetComponent<SegmentPhysics>().id = segments.Count;
-            if (segments.Count >= 1)
-                segment_tempo.transform.position = positionSegment();
-            else
-                segment_tempo.transform.position = spawn_position.position;
 
+            segment_tempo.transform.position = positionSegment(spawn_direction);
             segment_tempo.transform.parent = segment_parent.transform;
             segments.Add(segment_tempo);
         }
     }
 
-    Vector3 positionSegment()
+    Vector3 positionSegment(Vector3 spawn_direction)
     {
         int size = segments.Count;
-        Vector3 position = segments[size - 1].transform.position + new Vector3(0f, -transform.localScale.y, 0f);
+        if(size < 1)
+            return spawn_start.position;
+        
+        Vector3 position = segments[size - 1].transform.position + (spawn_direction * Segment.transform.localScale.y);
         return position;
     }
 
